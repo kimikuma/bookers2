60e1423180c07d611176eb46075a0b4dfa856a2b
EC2 Default User <kimikuma>
2024-10-08 13:13:51 +0000

    更新

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action:configure_permitted_parameters,if: :devise_controller?
 
-  #def after_sign_up_path_for(resource)
- # end
+  def after_sign_up_path_for(resource)
+    user_path(user.id)
+  end
 
-  #def after_singn_out_path_for(resource)
-  #end
+  def after_singn_out_path_for(resource)
+    user_path(user.id)
+  end
 
   protected
 
