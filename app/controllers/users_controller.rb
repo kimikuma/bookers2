e97586a1a7c2a7d4ac62e3f48331ab2ca1ab7708
EC2 Default User <kimikuma>
2024-10-08 12:29:32 +0000

    更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,11 @@ class UsersController < ApplicationController
   def update
     user=User.find(params[:id])
     user.update(user_params)
-    redirect_to user_path(@user.id)
+    redirect_to user_path(user.id)
   end
+
+  private
+   def user_params
+     params.require(:user).permit(:name, :profile_image, :introduction)
+   end
 end
