654d1b2737c7d5ea4dad681efa683b253235c8a2
EC2 Default User <kimikuma>
2024-10-09 14:53:55 +0000

    [final]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,10 +18,10 @@ class UsersController < ApplicationController
   def update
 
     is_matching_login_user
-    user=User.find(params[:id])
-     if user.update(user_params)
+    @user=User.find(params[:id])
+     if @user.update(user_params)
        flash[:notice]="You have updated user successfully."
-       redirect_to user_path(user.id)
+       redirect_to user_path(@user.id)
      else
        render :edit
      end
